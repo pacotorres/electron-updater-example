25d551087e107623fff31913a22f77bd36805c50
Paco Torres <javierspider_@hotmail.com>
2022-09-17 22:20:42 -0500

    Actualizacion

--- main.js
+++ main.js
@@ -2,35 +2,59 @@ const {app, BrowserWindow} = require('electron')
 const path = require ('path')
 const {autoUpdater} = require('electron-updater')
 const log = require('electron-log');
-log.transports.file.resolvePath = () => path.join(__dirname, '/logs/main.log');
-log.info('Hello, log');
-log.warn('Some problem appears');
- 
+log.transports.file.resolvePath = () => path.join('C:\\Users\\javie\\Documents\\Github\\electron-updater-example', '/logs/main.log');
+autoUpdater.logger = log
+log.log('App version: '+app.getVersion());
+//autoUpdater.checkForUpdates()
 let win;
 
 function createWindow() {
-    win = new BrowserWindow({width:300,height:400})
+    win = new BrowserWindow({
+        width:300,
+        height:400,
+        webPreferences:{
+            nodeIntegration:false
+        }
+    })
     win.loadFile(path.join(__dirname,'index.html'))
+    win.webContents.executeJavaScript(`document.querySelector('#version').innerText='${app.getVersion()}'`)
+
+    win.on('ready-to-show', () => {
+        autoUpdater.checkForUpdates()
+        console.log('Entre')
+    });
 }
 
-app.on('ready',()=>{
-    console.log(__dirname)
-    createWindow()
-    autoUpdater.checkForUpdatesAndNotify()
+autoUpdater.on('checking-for-update',()=>{
+    log.info('checking-for-update...')
+    win.webContents.executeJavaScript(`document.querySelector('#act').innerText='Checando Actualización'`)
 })
 
 autoUpdater.on('update-available',()=>{
     log.info('update-available')
+    win.webContents.executeJavaScript(`document.querySelector('#act').innerText='Actualización Disponible'`)
 })
 
-autoUpdater.on('checking-for-update',()=>{
-    log.info('checking-for-update')
+autoUpdater.on('update-not-available',()=>{
+    log.info('update-not-available')
+    win.webContents.executeJavaScript(`document.querySelector('#act').innerText='Sin actualización'`)
 })
 
-autoUpdater.on('download-progress',()=>{
+autoUpdater.on('error',(error)=>{
+    log.info('Error: '+error)
+})
+
+autoUpdater.on('download-progress',(progressTrack)=>{
     log.info('download-progress')
+    log.info(progressTrack)
+    win.webContents.executeJavaScript(`document.querySelector('#act').innerText='${progressTrack.percent}'`)
 })
 
 autoUpdater.on('update-downloaded',()=>{
     log.info('update-downloaded')
+    win.webContents.executeJavaScript(`document.querySelector('#act').innerText='Cierre el programa para actualizar'`)
+})
+
+app.on('ready',()=>{
+    createWindow()
 })
